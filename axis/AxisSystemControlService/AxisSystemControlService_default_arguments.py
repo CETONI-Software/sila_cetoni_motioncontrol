21d1a2aaf252d8ffcc7cfe9da6f8e9e4f2366668
Florian Meinicke <florian.meinicke@cetoni.de>
2020-12-15 09:32:20 +0100

    Add generated code for axis system features
    Change-Id: I04087fa17f97a4580893ae74210610e4bd531e48

---
+++ axis/AxisSystemControlService/AxisSystemControlService_default_arguments.py
@@ -0,0 +1,52 @@
+# This file contains default values that are used for the implementations to supply them with 
+#   working, albeit mostly useless arguments.
+#   You can also use this file as an example to create your custom responses. Feel free to remove
+#   Once you have replaced every occurrence of the defaults with more reasonable values.
+#   Or you continue using this file, supplying good defaults..
+
+# import the required packages
+import sila2lib.framework.SiLAFramework_pb2 as silaFW_pb2
+import sila2lib.framework.SiLABinaryTransfer_pb2 as silaBinary_pb2
+from .gRPC import AxisSystemControlService_pb2 as pb2
+
+# initialise the default dictionary so we can add keys. 
+#   We need to do this separately/add keys separately, so we can access keys already defined e.g.
+#   for the use in data type identifiers
+default_dict = dict()
+
+
+default_dict['EnableAxisSystem_Parameters'] = {
+    
+}
+
+default_dict['EnableAxisSystem_Responses'] = {
+    
+}
+
+default_dict['DisableAxisSystem_Parameters'] = {
+    
+}
+
+default_dict['DisableAxisSystem_Responses'] = {
+    
+}
+
+default_dict['ClearAxisFaultState_Parameters'] = {
+    
+}
+
+default_dict['ClearAxisFaultState_Responses'] = {
+    
+}
+
+default_dict['Subscribe_AxisSystemState_Responses'] = {
+    'AxisSystemState': silaFW_pb2.String(value='default string')
+}
+
+default_dict['Subscribe_AxisFaultState_Responses'] = {
+    'AxisFaultState': silaFW_pb2.Boolean(value=False)
+}
+
+default_dict['Get_FCPAffectedByMetadata_AxisIdentifier_Responses'] = {
+    'AffectedCalls': [silaFW_pb2.String(value='default string')]
+}
